f04f9f7971f1db1a7120c9f084ce21130b708f1e
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-06-10 00:09:51 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace En_un_dia__Ejercicio_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que lea una letra tecleada por el usuario y diga si se trata de 
+            //una vocal, una cifra numerica o una constante. Tips: usar dato tipo "char".
+            char let;
+            Console.WriteLine("Ingresar Valor:");
+            let = Convert.ToChar(Console.ReadLine());
+
+            switch (let)
+            {
+                case ' ': Console.WriteLine("Es un Espacio"); break;
+                case 'e': goto case 'a';
+                case 'i': goto case 'a';
+                case 'o': goto case 'a';
+                case 'u': goto case 'a';
+                case 'a': Console.WriteLine("Es una vocal");  break;
+
+                case '1': goto case '0';
+                case '2': goto case '0';
+                case '3': goto case '0';
+                case '4': goto case '0';
+                case '5': goto case '0';
+                case '6': goto case '0';
+                case '7': goto case '0';
+                case '8': goto case '0';
+                case '9': goto case '0';
+                case '0': Console.WriteLine("Es una cifra numerica"); break;
+                default: Console.WriteLine("Es una Constante"); break;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
