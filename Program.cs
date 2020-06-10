3787af7ddb533e0c1fe0fbb74815a2ff10db9428
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-06-09 23:31:27 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace En_un_dia__Ejercicio_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Usar el operador condicional para calcular el menor de dos numeros.
+            int num;
+            int num1;
+            Console.WriteLine("Ingresar num 1");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresar num 2");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num < num1)
+            {
+                Console.WriteLine(num + " Es el menor");
+            }
+            else if (num1 < num)
+            { 
+                Console.WriteLine(num1 + " Es el menor");
+            }
+            Console.ReadLine();
+        }
+    }
+}
