4288389532d935bc63376aaa1f1a9b10c68fc09c
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-06-09 23:38:24 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace En_un_dia__Ejercicio_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que escriba en pantalla los numeros pares
+            //del 23 al 7 en orden descendente, usando while.
+            int n = 23;
+            
+            do
+            {
+                n = n - 1;
+                Console.WriteLine(n);
+                n = n - 1;
+            } while (n > 7 );
+            
+            Console.ReadKey();
+        }
+    }
+}
